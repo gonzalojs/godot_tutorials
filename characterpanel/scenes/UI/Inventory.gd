2aebf4ed7862751bc7b10d7d2ab20fad46a16079
gonzalojs <eogutier@gmail.com>
2021-03-03 20:34:53 -0300

    singletones

--- characterpanel/scenes/UI/Inventory.gd
+++ characterpanel/scenes/UI/Inventory.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 		var inv_slot_new = template_inv_slot.instance()
 		if PlayerData.inv_data[i]["Item"] != null:
 			var item_name = GameData.item_data[str(PlayerData.inv_data[i]["Item"])]["Name"]
-			var icon_texture = load(direccion + item_name + '.png') #aqui van los assets de íconos
+			var icon_texture = load("res://assets/Icon_Items/" + item_name + '.png') #aqui van los assets de íconos
 		gridcontainer.add_child(inv_slot_new, true)
