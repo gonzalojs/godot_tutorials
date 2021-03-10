054dedb814645e39563ec0941843901378e57937
gonzalojs <eogutier@gmail.com>
2021-03-10 00:34:36 -0300

    tiles

--- characterpanel/scenes/UI/EquipmentSlot.gd
+++ characterpanel/scenes/UI/EquipmentSlot.gd
@@ -3,17 +3,17 @@ extends TextureRect
 func get_drag_data(_position: Vector2):
 	var data = {}
 	data["origin_texture"] = texture
-	
+
 	var drag_texture = TextureRect.new()
 	drag_texture.expand = true
 	drag_texture.texture = texture
 	drag_texture.rect_size = Vector2(100, 100)
-	
+
 	var control = Control.new()
 	control.add_child(drag_texture)
 	drag_texture.rect_position = -0.5 * drag_texture.rect_size
 	set_drag_preview(control)
-	
+
 	return data
 
 func can_drop_data(_position: Vector2, data) -> bool:
